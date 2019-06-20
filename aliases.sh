59948e79ab00b98d54f004f3fdf319c0a269c50f
icenold <arnold.naval99@gmail.com>
2019-06-19 21:05:22 -0600

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -7,6 +7,13 @@ alias gp='git pull';
 alias endserve='git pull && powershell ./start_server.ps1';
 alias gitprettylog='git log --pretty="%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s"';
 alias gitgraph="git log --all --graph --pretty=format:'%C(auto)%h%C(auto)%d %s %C(Blue)(%aN, %ar)'";
+alias icegraph="git log --all --graph --pretty=format:'%C(auto)%h%C(auto)%d %s %C(Blue)(%aN, %ar)'";
+icediff(){
+  git difftool --dir-diff --no-prompt $1~1 $1;
+};
 alias clear='printf "\033c"';
 
 bind TAB:menu-complete;
+
+
+export PATH=$PATH:"/C/Program Files (x86)/Meld/lib";
